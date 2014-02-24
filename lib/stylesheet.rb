378b085c66f3874aeab16f8eaa16a71591e5f1ee
Chris Temple <temple3188@gmail.com>
2014-02-24 20:09:23 +0000

    Doing some refactoring separating stylesheets logic from unused css

--- lib/stylesheet.rb
+++ lib/stylesheet.rb
@@ -15,3 +15,28 @@ class Stylesheet
     @parser.each_selector { |styles| @styles << styles }
   end
 end
+
+class Stylesheets
+  attr_accessor :stylesheets
+
+  def initialize
+    @stylesheets = []
+  end
+
+  def add uris
+    uris = Array(uris)
+    uris.each { |uri| @stylesheets << Stylesheet.new(uri) unless already_included? uri }
+  end
+
+  def already_included? uri
+    @stylesheets.any? { |stylesheet| stylesheet.uri == uri }
+  end
+
+  def stylesheet(uri)
+    @stylesheets.find { |stylesheet| stylesheet.uri == uri }
+  end
+
+  def styles
+    @stylesheets.inject(Set.new) {|styles, stylesheet| styles.merge stylesheet.styles }
+  end
+end
--- lib/unused_css.rb
+++ lib/unused_css.rb
@@ -4,27 +4,7 @@ class UnusedCSS
   attr_accessor :stylesheets
 
   def initialize
-    @stylesheets = []
-  end
-
-  def add_stylesheet uri
-    @stylesheets << Stylesheet.new(uri) unless stylesheet_exists? uri
-  end
-
-  def stylesheet_exists? uri
-    @stylesheets.any? { |stylesheet| stylesheet.uri == uri }
-  end
-
-  def add_stylesheets uris
-    uris.each { |uri| add_stylesheet uri }
-  end
-
-  def stylesheet(stylesheet_uri)
-    @stylesheets.find { |stylesheet| stylesheet.uri == stylesheet_uri }
-  end
-
-  def styles
-    @stylesheets.inject(Set.new) {|styles, stylesheet| styles.merge stylesheet.styles }
+    @stylesheets = Stylesheets.new
   end
 
   def watch!(watir_browser)
@@ -33,10 +13,10 @@ class UnusedCSS
       def goto(uri)
         super(uri)
         stylesheets = elements(tag_name: 'link').map {|stylesheet| stylesheet.attribute_value('href') }
-        @unused_css.add_stylesheets stylesheets
+        @unused_css.stylesheets.add stylesheets
         stylesheets.each do |stylesheet|
-          @unused_css.stylesheet(stylesheet).styles.each do |style|
-            @unused_css.stylesheet(stylesheet).styles.delete(style) if self.element(css: style).exist?
+          @unused_css.stylesheets.stylesheet(stylesheet).styles.each do |style|
+            @unused_css.stylesheets.stylesheet(stylesheet).styles.delete(style) if self.element(css: style).exist?
           end
         end
       end
